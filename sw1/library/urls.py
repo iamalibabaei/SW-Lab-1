ad0a9f87d0152308e330e116b49cbbe06dc2d6db
ali <alibabaei037@gmail.com>
2022-03-09 13:34:10 +0330

    feat(book_creation): Add book creation views, urls and api

--- sw1/library/urls.py
+++ sw1/library/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from library.views import index
+from library.views import index, create_book
 
 urlpatterns = [
     path('', index),
+    path('add', create_book)
 ]
