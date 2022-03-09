0dd5ddac53734356342522419d25346eba4b1200
ali <alibabaei037@gmail.com>
2022-03-09 12:57:58 +0330

    add: template dir

--- sw1/sw1/settings.py
+++ sw1/sw1/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sw1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'sw1/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
